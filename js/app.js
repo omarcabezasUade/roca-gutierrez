36bea4302a6ba0167b3881b77a3ed7c6295a8bbf
omarcabezasUade <ocabezas@uade.edu.ar>
2026-05-25 22:54:20 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,8 @@
+function mostrarMensaje(){
+
+    const mensaje = document.getElementById("mensaje");
+
+    mensaje.textContent =
+    "Para consultar el horario completo se recomienda verificar el cuadro oficial actualizado.";
+
+}
